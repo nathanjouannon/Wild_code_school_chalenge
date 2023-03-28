564a7ec7b45b96ff964b34c5d3faab4d39bccc40
Jouannon Nathan <nathan.jouannon@gmail.com>
2023-03-28 14:29:10 +0200

    gestion de l'affiche des nom en DB

--- frontend/src/components/List.js
+++ frontend/src/components/List.js
@@ -1,11 +1,27 @@
-import '../styles/List.css'
+import React from 'react';
+import '../styles/List.css';
+
+
+function AfficheListDB() {
+    fetch('http://localhost:4200/api/names')
+        .then(response => response.json())
+        .then(data => {
+            return (
+                data.map((name) => (
+                    <div className='wcs-list-item'>name</div>
+                ))
+            )
+        })
+        .catch(error => {console.log(error)})
+}
 
 function List() {
     return(
         <div className="wcs-list">
             <h2 className="wcs-list-title"> Membres de l'équipage</h2>
             <div className="wcs-list-container">
-                {/* remplacer ici par la liste retournée par le server */}
+                
+                <AfficheListDB />
                 
 
                 <div className="wcs-list-item">Eleftheria</div>
