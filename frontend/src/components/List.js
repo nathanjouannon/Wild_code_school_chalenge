47945434470d87a87fa53717faab70e72e50e9bd
Jouannon Nathan <nathan.jouannon@gmail.com>
2023-03-31 15:24:58 +0200

    réecriture propre

--- frontend/src/components/List.js
+++ frontend/src/components/List.js
@@ -5,30 +5,29 @@ import axios from 'axios'
 
 
 
+
 function List() {
 
     const [names, setNames] = useState([]);
 
     useEffect(() => {
-        axios.get('http://localhost:4200/api/names')
-          .then(response => {
-            const namesArray = response.data.names.map(nameObj => nameObj.name);
-            setNames(namesArray)
-          })
-          .catch(error => {
-            console.log(error);
-          });
-      }, []);
+          axios.get('http://localhost:4200/api/names', { headers: { "Content-Type": "application/json" }})
+            .then(response => {
+              setNames(response.data.names)
+            })
+            .catch(error => {
+              console.log(error);
+            });
+        }, []);
 
-    const nameList = names.map((element, index) => 
-        <a className='wcs-list-item' key={index} href={"http://localhost:3000/"+index}>{element}</a>
-    );
 
     return(
         <div className="wcs-list">
             <h2 className="wcs-list-title"> Membres de l'équipage</h2>
             <div className="wcs-list-container">
-                {nameList}
+                {names.map((eltName) => (
+                  <div className='wcs-list-item' key={eltName._id}>{eltName.name}</div>
+                ))}
             </div>
         </div>
     )
